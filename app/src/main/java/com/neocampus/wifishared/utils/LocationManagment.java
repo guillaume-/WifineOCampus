2e251760cec1805504e6f8eac1b331cf37c0f9ea
SIAMEi3E <siame.ieee@gmail.com>
2017-02-17 16:02:53 +0100

    Version 1.0.0

--- app/src/main/java/com/neocampus/wifishared/utils/LocationManagment.java
+++ app/src/main/java/com/neocampus/wifishared/utils/LocationManagment.java
@@ -1,17 +1,20 @@
 package com.neocampus.wifishared.utils;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 
 import java.util.List;
 
 /**
  * Created by Guillaume RIPOLL on 26/01/2017.
  */
-public class LocationManagment {
+public class LocationManagment implements LocationListener {
 
     private Location MetroUPS;
     private LocationManager locationManager;
@@ -24,24 +27,24 @@ public class LocationManagment {
         MetroUPS.setLongitude(1.4630574000000252);
         try {
             locationManager = (LocationManager) c.getSystemService(Context.LOCATION_SERVICE);
-        } catch (SecurityException e) {}
+        } catch (SecurityException e) {
+        }
     }
 
-    public boolean isEnabled()
-    {
-        return locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER );
+    public boolean isEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 
-    public boolean isAtUniversity(){
+    public boolean isAtUniversity() {
         // getting GPS status
         boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
         // getting network status
         boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        if(!isGPSEnabled && !isNetworkEnabled)
+        if (!isGPSEnabled && !isNetworkEnabled)
             return false;
-        else{
+        else {
             Location loc = find_Location();
-            if(loc != null){
+            if (loc != null) {
                 float distance = loc.distanceTo(MetroUPS);
                 boolean res = distance <= 2000.f; //in meters
                 return res;
@@ -52,6 +55,12 @@ public class LocationManagment {
 
     public Location find_Location() {
         Location loc = null;
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return loc;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         List<String> providers = locationManager.getProviders(true);
         try {
             for (String provider : providers) {
@@ -67,5 +76,25 @@ public class LocationManagment {
             }
         }catch(SecurityException e){}
         return loc;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
