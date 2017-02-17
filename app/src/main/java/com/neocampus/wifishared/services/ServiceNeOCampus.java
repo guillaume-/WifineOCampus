961bd868cc18b3efe6149bc6ba573fc01a8e3ee5
SIAMEi3E <siame.ieee@gmail.com>
2017-02-17 23:26:25 +0100

    Version 1.0.0

--- app/src/main/java/com/neocampus/wifishared/services/ServiceNeOCampus.java
+++ app/src/main/java/com/neocampus/wifishared/services/ServiceNeOCampus.java
@@ -35,26 +35,93 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
+/**
+ * ServiceNeOCampus est une classe de service générale de l'application.
+ * <p>
+ *     Le service contient tous les services d'arrière plan de l'application et
+ *     s'occupe de leur lancement ou de leur arrêt, tous les services sont associés a un observateur d'état.
+ * </p>
+ */
 public class ServiceNeOCampus extends Service implements OnServiceSetListener, Observer {
 
+    /**
+     * {@link Binder} permet de se connecté au service afin de communiquer avec cell-ci
+     */
     private ServiceNeOCampusBinder oCampusBinder;
+
+    /**
+     * Objet permettant de communication avec la base de données
+     */
     private SQLManager sqlManager;
+
+    /**
+     * Objet permettant de savoir si le fournisseur de la borne se trouve dans le campus UPS
+     */
     private LocationManagment locationManagment;
 
+    /**
+     * Observable permettant d'être notifié sur un évènnement de connexion
+     */
     private ClientObservable clientObservable;
+
+    /**
+     * Observable permettant d'être notifié sur un changement de la configuration WIFI-AP
+     */
     private HotspotObservable hotspotObservable;
+
+    /**
+     * Observable permettant d'être notifié sur un changement du niveau de la batterie
+     */
     private BatterieObservable batterieObservable;
+
+    /**
+     * Observable permettant d'être notifié sur un changement de la consommation de données
+     */
     private DataObservable dataObservable;
+
+    /**
+     * Observable permettant d'être notifié sur un déclenchement d'alarme
+     */
     private TimeObservable timeObservable;
+
+    /**
+     * Observable permettant d'être notifié sur un changement de l'accès internet
+     */
     private NetworkObservable networkObservable;
 
+    /**
+     * Service d'observation de la consommation de données
+     */
     private ServiceDataTraffic serviceData;
+
+    /**
+     * Service d'observation de la connexion au partage
+     */
     private ServiceTaskClients serviceTaskClients;
+
+    /**
+     * Service d'observation de la configuration WIFI-AP
+     */
     private OnHotspotReceiver onHotspotReceiver;
+
+    /**
+     * Service d'observation du niveau de la batterie
+     */
     private OnBatterieReceiver onBatterieReceiver;
+
+    /**
+     * Service de gestion du temps d'activation du partage
+     */
     private OnAlarmReceiver onAlarmReceiver;
+
+    /**
+     * Service d'observation de l'accès internet
+     */
     private OnNetworkReceiver onNetworkReceiver;
 
+    /**
+     * Constructeur du service générale
+     */
     public ServiceNeOCampus() {
         this.oCampusBinder = new ServiceNeOCampusBinder();
         this.hotspotObservable = new HotspotObservable();
@@ -66,6 +133,9 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
     }
 
 
+    /**
+     * Cette méthode initialise les services
+     */
     @Override
     public void onCreate() {
         this.locationManagment = new LocationManagment(this);
@@ -95,11 +165,21 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
         super.onCreate();
     }
 
+    /**
+     * Permet le redémarrage du service en cas de fermetture de l'interface de l'application
+     * @param intent
+     * @param flags
+     * @param startId
+     * @return identifiant permettant d'indiquer que le service doit se relancer
+     */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return Service.START_STICKY;
     }
 
+    /**
+     * Permet la désactivation complète les services
+     */
     @Override
     public void onDestroy() {
         this.serviceData.stopWatchDog();
@@ -113,17 +193,30 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
         super.onDestroy();
     }
 
+    /**
+     * Installe un {@link Binder} permettant la communication avec ce service
+     * @param intent
+     * @return return le binder a installer
+     */
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return oCampusBinder;
     }
 
+    /**
+     * permet de désinstaller le {@link Binder} permettant la communication
+     * @param conn {@link Binder} à désinstaller
+     */
     @Override
     public void unbindService(ServiceConnection conn) {
         super.unbindService(conn);
     }
 
+    /**
+     * Cette méthode permet de s'inscrire sur la liste des notifiés de tous les observables
+     * @param observer objet recevant la notification
+     */
     @Override
     public void addObserver(Observer observer) {
         batterieObservable.addObserver(observer);
@@ -134,6 +227,10 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
         timeObservable.addObserver(observer);
     }
 
+    /**
+     * Cette méthode permet de se désinscrire de la liste des notifiés de tous les observables
+     * @param observer objet recevant la notification
+     */
     @Override
     public void removeObserver(Observer observer) {
         batterieObservable.deleteObserver(observer);
@@ -145,32 +242,54 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
     }
 
 
+    /**
+     * Cette méthode actualise sur l'interface graphique la consommation de données si l'utilisateur en fait la demande
+     * @param listener interface de communication avec fragment
+     */
     @Override
     public void peekDataTraffic(OnFragmentSetListener listener) {
         listener.onRefreshDataTraffic(dataObservable.getValue());
     }
 
+    /**
+     * Cette méthode actualise sur l'interface graphique les clients connectés depuis le début du partage
+     * @param listener interface de communication avec fragment
+     */
     @Override
     public void peekAllClients(OnReachableClientListener listener) {
         listener.onReachableClients(clientObservable.getHistoriqueClients());
     }
 
+    /**
+     * Cette méthode actualise sur l'interface graphique les clients actuellement connectés
+     * @param listener interface de communication avec fragment
+     */
     @Override
     public void peekReachableClients(OnReachableClientListener listener) {
         listener.onReachableClients(clientObservable.getClients());
     }
 
+    /**
+     * Permet de reinitialiser le service d'observation de la consommation de données
+     */
     @Override
     public void resetBaseT0() {
         dataObservable.setValue(0);
         serviceData.refreshFromDataBase(0);
     }
 
+    /**
+     * Cette méthode actualise sur l'interface graphique le temps pour lequel l'alarme se déclenchera
+     * @param listener interface de communication avec fragment
+     */
     @Override
     public void peekTimeValue(OnFragmentSetListener listener) {
         listener.onRefreshTimeValue(timeObservable.getDate());
     }
 
+    /**
+     * Cette méthode permet de sauvegarder en urgence la consommation actuelle de données
+     */
     @Override
     public void storeInDataBase() {
         sqlManager.setConfigurationS(true);
@@ -178,6 +297,9 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
         sqlManager.setConfigurationD(dataT0);
     }
 
+    /**
+     * Cette méthode permet de restaurer la consommation de données sauvegardé en urgence
+     */
     public void restoreFromDataBase() {
         TableConfiguration configuration = sqlManager.getConfiguration();
         if(configuration.isStored()) {
@@ -196,18 +318,28 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
         serviceData.refreshFromDataBase(configuration.getDataT0());
     }
 
+    /**
+     * Cette méthode permet de lancer tous les services d'observations
+     */
     private void startWatchDog() {
         this.serviceData.startWatchDog(1000);
         this.serviceTaskClients.startWatchDog(10000);
         this.onAlarmReceiver.startAlarm(this, sqlManager);
     }
 
+    /**
+     * Cette méthode permet de d'arrêter tous les services d'observations
+     */
     private void stopWatchDog() {
         this.serviceData.stopWatchDog();
         this.serviceTaskClients.stopWatchDog();
         this.onAlarmReceiver.stopAlarm(this, sqlManager);
     }
 
+    /**
+     * Cette méthode permet d'initialiser une ligne d'une session de partage
+     * dans la base de données
+     */
     private void createSession() {
         if(hotspotObservable.getSessionId() == -1) {
             long date = new Date().getTime();
@@ -218,6 +350,10 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
         }
     }
 
+    /**
+     * Cette méthode permet de pré-configurer le démarrage et l'arrêt d'un partage
+     * @param enable etat du partage
+     */
     public void setWatchDogState(boolean enable) {
         if (enable) {
             createSession();
@@ -228,6 +364,10 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
         }
     }
 
+    /**
+     * Cette méthode permet de sauvegarder dans
+     * la base de données tous les informations observées avant l'arrêt d'un partage
+     */
     public void saveInDataBase(){
         int idConso =
                 hotspotObservable.getSessionId();
@@ -242,22 +382,41 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
         }
     }
 
+    /**
+     * Cette méthode teste si le temps d'activation du
+     * partage a été dépassé au déclenchement d'une alarme
+     * @param timeValue temps au déclenchement de l'alarme
+     * @return vrai si temps d'activation s'est écoulé faux sinon
+     */
     public boolean isOverTimeLimit(long timeValue) {
         TableConfiguration tableConfiguration = sqlManager.getConfiguration();
         return tableConfiguration.getDateAlarm() > 0
                 && tableConfiguration.getDateAlarm() < timeValue;
     }
 
+    /**
+     * Cette méthode teste si la consommation de données a atteint la limite définie par l'utilisateur
+     * @param dataLevel consommation actuelle de données
+     * @return vrai si le seuil de consommation de données a été atteint faux sinon
+     */
     public boolean isOverDataLimit(long dataLevel) {
         TableConfiguration tableConfiguration = sqlManager.getConfiguration();
         return tableConfiguration.getLimiteConsommation() <= dataLevel;
     }
 
+    /**
+     * Cette méthode teste si le niveau de la batterie a atteint la limite définie par l'utilisateur
+     * @param level niveau actuel de la batterie
+     * @return
+     */
     public boolean isOverBatterieLimit(int level) {
         TableConfiguration tableConfiguration = sqlManager.getConfiguration();
         return tableConfiguration.getLimiteBatterie() >= level;
     }
 
+    /**
+     * Permet d'arrêter le partage
+     */
     private void stopHotpost() {
         if(WifiApControl.checkPermission(this)) {
             WifiApControl apControl = WifiApControl.getInstance(this);
@@ -267,6 +426,11 @@ public class ServiceNeOCampus extends Service implements OnServiceSetListener, O
         }
     }
 
+    /**
+     * Cette méthode est appelé par les observables pour notifier un changement
+     * @param o observable source de la notification
+     * @param arg information sur le changement observé
+     */
     @Override
     public void update(Observable o, Object arg) {
         if (o instanceof HotspotObservable) {
